4601e5dd1251fb899192a03537b1e1eac389ec5e
An Lu <anlu0418@gmail.com>
2024-06-24 18:49:00 -0500

    Add files via upload

---
+++ Resources/schema.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS CATERGORY;
+DROP TABLE IF EXISTS SUBCATERGORY;
+DROP TABLE IF EXISTS CAMPAIGN;
+DROP TABLE IF EXISTS CONTACTS;
+
+CREATE TABLE category (
+    category_id VARCHAR(150) PRIMARY KEY,
+    category VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Contacts (
+    contact_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(150) PRIMARY KEY,
+    category VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE campaign (
+    cf_id SERIAL PRIMARY KEY,
+    contact_id SERIAL NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    goal DOUBLE PRECISION NOT NULL,
+    pledged DOUBLE PRECISION NOT NULL,
+    outcome VARCHAR(100) NOT NULL,
+    backers_count INT NOT NULL,
+    country VARCHAR(100) NOT NULL,
+    currency VARCHAR(100) NOT NULL,
+    launched_date DATE,
+    end_date DATE,
+    category_id VARCHAR (150) NOT NULL,
+    subcategory_id VARCHAR(150) not null,
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
